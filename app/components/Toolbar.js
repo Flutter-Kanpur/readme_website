472c14e4e3fcf4a6d5bddd579563afeb73aa57ba
pushtisonawala <pushtisonawala24@gmail.com>
2026-01-30 00:33:25 +0530

    toolbar button

--- app/components/Toolbar.js
+++ app/components/Toolbar.js
@@ -1,71 +1,43 @@
+
 import Icon from './Icon';
 import './Toolbar.css';
 
-export default function Toolbar({ onBold, onItalic, onUnderline, onQuote, onLink, onImage }) {
-  const handleMouseDown = (e, callback) => {
+function ToolbarButton({ onClick, className, title, type = 'button', children }) {
+  const handleMouseDown = (e) => {
     e.preventDefault();
-    callback();
+    onClick();
   };
+  return (
+    <button
+      onMouseDown={handleMouseDown}
+      className={className}
+      title={title}
+      type={type}
+    >
+      {children}
+    </button>
+  );
+}
 
+export default function Toolbar({ onBold, onItalic, onUnderline, onQuote, onLink, onImage }) {
   return (
     <div className="toolbar">
-
       <div className="toolbar-group">
-        <button 
-          onMouseDown={(e) => handleMouseDown(e, onBold)}
-          className="toolbar-btn toolbar-btn-bold"
-          title="Bold (Ctrl+B)"
-          type="button"
-        >
-          B
-        </button>
-        <button 
-          onMouseDown={(e) => handleMouseDown(e, onItalic)}
-          className="toolbar-btn toolbar-btn-italic"
-          title="Italic (Ctrl+I)"
-          type="button"
-        >
-          I
-        </button>
-        <button 
-          onMouseDown={(e) => handleMouseDown(e, onUnderline)}
-          className="toolbar-btn toolbar-btn-underline"
-          title="Underline (Ctrl+U)"
-          type="button"
-        >
-          U
-        </button>
+        <ToolbarButton onClick={onBold} className="toolbar-btn toolbar-btn-bold" title="Bold (Ctrl+B)">B</ToolbarButton>
+        <ToolbarButton onClick={onItalic} className="toolbar-btn toolbar-btn-italic" title="Italic (Ctrl+I)">I</ToolbarButton>
+        <ToolbarButton onClick={onUnderline} className="toolbar-btn toolbar-btn-underline" title="Underline (Ctrl+U)">U</ToolbarButton>
       </div>
-
       <div className="toolbar-group">
-        <button 
-          onMouseDown={(e) => handleMouseDown(e, onQuote)}
-          className="toolbar-btn toolbar-btn-icon"
-          title="Quote"
-          type="button"
-        >
+        <ToolbarButton onClick={onQuote} className="toolbar-btn toolbar-btn-icon" title="Quote">
           <Icon src="/assets/icons/quote.png" alt="Quote" />
-        </button>
-
-        <button 
-          onMouseDown={(e) => handleMouseDown(e, onLink)}
-          className="toolbar-btn toolbar-btn-icon"
-          title="Link"
-          type="button"
-        >
+        </ToolbarButton>
+        <ToolbarButton onClick={onLink} className="toolbar-btn toolbar-btn-icon" title="Link">
           <Icon src="/assets/icons/link.png" alt="Link" />
-        </button>
-
-        <button 
-          onMouseDown={(e) => handleMouseDown(e, onImage)}
-          className="toolbar-btn toolbar-btn-icon"
-          title="Image"
-          type="button"
-        >
+        </ToolbarButton>
+        <ToolbarButton onClick={onImage} className="toolbar-btn toolbar-btn-icon" title="Image">
           <Icon src="/assets/icons/image.png" alt="Image" />
-        </button>
+        </ToolbarButton>
       </div>
-
     </div>
   );
 }
