b02a016b870ce6ac46a8dd17f62ec702d92d5e84
drishtant-ranjan-landmark <drishtant@stylishop.com>
2026-08-17 02:16:24 +0530

    fix

--- app/robots.js
+++ app/robots.js
@@ -5,10 +5,15 @@ export default function robots() {
   return {
     rules: {
       userAgent: '*',
-      allow: ['/blogs/', '/blogs'],
-      disallow: ['/blogs/login', '/blogs/register', '/blogs/drafts', '/blogs/write', '/blogs/edit/'],
+      allow: '/blogs/',
+      disallow: [
+        '/blogs/login',
+        '/blogs/register',
+        '/blogs/drafts',
+        '/blogs/write',
+        '/blogs/edit/',
+      ],
     },
     sitemap: `${SITE_ORIGIN}/sitemap.xml`,
-    host: SITE_ORIGIN,
   };
 }
